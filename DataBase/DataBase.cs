6db2257f1f8b4d4422f41638d15e230c814df2ca
vadimburnaeff1 <sergey_like99@mail.ru>
2019-11-20 01:44:16 +0300

    доделал базу данных

--- DataBase/DataBase.cs
+++ DataBase/DataBase.cs
@@ -8,10 +8,9 @@ using System.Data.Linq;
 
 namespace DataBase
 {
-
-	
 	public interface IDataBase
 	{
+		int GetId(string username);
 		void AddUser(string user);
 
 		void AddMsg(string Msg);
@@ -22,12 +21,43 @@ namespace DataBase
 
 		string ModificationMsgDB(int idMsg, string newMsg);
 
+		bool HaveMsg(string userName);
+
 	}
 
 	public class DataBase : IDataBase
 	{
 		private static string[] UnsentMsg;
 
+		public int GetId(string username)
+		{
+			int iden = 0;
+			string name = username;
+			DataClasses1DataContext db = new DataClasses1DataContext();
+	
+			foreach (var user in db.GetTable<User>().OrderByDescending(u => u.Id))
+			{
+				if (user.name == name)
+				{
+					iden = user.Id;
+				}
+			}
+			return iden;
+		}
+
+		public bool HaveMsg(string userName)
+		{
+			int b = GetId(userName);
+			DataClasses1DataContext db2 = new DataClasses1DataContext();
+			foreach (var user in db2.GetTable<Message>().OrderByDescending(u => u.Id2))
+			{
+				if (user.Id2 == b)
+				{
+					return true;
+				}
+			}
+		    return false;
+		}
 		public DataBase()
 		{
 			UnsentMsg = new string[3] { " ", " ", " " };
@@ -36,9 +66,8 @@ namespace DataBase
 
 		public void AddUser(string user)
 		{
-			//string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\228\Desktop\WOZAP\DataBase\Database1.mdf;Integrated Security=True";
 			DataClasses1DataContext db = new DataClasses1DataContext();
-			User user1 = new User { name = "user" };
+			User user1 = new User { name = user};
 			// добавляем его в таблицу Users
 			db.GetTable<User>().InsertOnSubmit(user1);
 			db.SubmitChanges();
@@ -49,16 +78,56 @@ namespace DataBase
 
 		}
 
+<<<<<<< HEAD
         /*string[] GetMsg(string userNameFrom, string userNameTo)
         {
 
 		}*/
+=======
+		public string[] GetMsg(string userNameFrom, string userNameTo)
+		{
+			//DataBase db = new DataBase();
+			int a;
+			int b;
+			a = GetId(userNameFrom);
+			b = GetId(userNameTo);
+			DataClasses1DataContext db2 = new DataClasses1DataContext();
+			int count = 0;
+			foreach (var user in db2.GetTable<Message>().OrderByDescending(u => u.Id2))
+			{
+				if (user.Id2 == b && user.Id1 == a)
+				{
+					count++;
+				}
+			}
+			//Console.WriteLine(count);
+
+			string[] str = new string[count];
+			int i = 0;
+			foreach (var user in db2.GetTable<Message>().OrderByDescending(u => u.Id1))
+			{
+				if (user.Id2 == b && user.Id1 == a)
+				{
+					if (i >= count)
+					{
+						string[] new_str = new string[count + 100];
+
+						for (int j = 0; j < count; j++)
+							new_str[j] = str[j];
+
+						count += 100;
+						str = new_str;
+					}
+					str[i++] = user.Msg;
+					//Console.WriteLine(str[i - 1]);
+				}
+			}
+			return str;
+		}
+>>>>>>> 3ebe067... доделал базу данных
 
 		public string[] GetUsers()
 		{
-			//string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\228\Desktop\WOZAP\DataBase\Database1.mdf;Integrated Security=True";
-
-			//string[] str = { "123", "123" };
 			DataClasses1DataContext db = new DataClasses1DataContext();
 
 			// Получаем таблицу пользователей
@@ -68,7 +137,6 @@ namespace DataBase
 			string[] str = new string[max_id];
 			foreach (var user in users)
 			{
-				//Console.WriteLine("{0} \t{1}", user.Id, user.name);
 				if (i >= max_id)
 				{
 					string[] new_str = new string[max_id + 100];
@@ -81,7 +149,6 @@ namespace DataBase
 				}
 
 				str[i++] = user.name;
-				//Console.WriteLine(str[i - 1]);
 			}
 			return str;
 		}
@@ -99,89 +166,19 @@ namespace DataBase
 
 	class Program
 	{
-		static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\228\Desktop\WOZAP\DataBase\Database1.mdf;Integrated Security=True";
 		static void Main(string[] args) 
 		{
-			DataContext db = new DataContext(connectionString);
-			//Console.WriteLine("До добавления");
-			foreach (var user in db.GetTable<Message>().OrderByDescending(u => u.Id1).Take(5))
-			{
-				Console.WriteLine("{0} \t{1} \t{2}", user.Id1, user.Id2, user.Msg);
-			}
-			//Console.WriteLine();
-
-			//// создаем нового пользователя
-			//User user1 = new User { name = "vadik" };
-			//// добавляем его в таблицу Users
-			//db.GetTable<User>().InsertOnSubmit(user1);
-			//db.SubmitChanges();
-
-			//Console.WriteLine();
-			//Console.WriteLine("После добавления");
-			//foreach (var user in db.GetTable<User>().OrderByDescending(u => u.Id).Take(5))
-			//{
-			//	Console.WriteLine("{0} \t{1}", user.Id, user.name);
-			//}
-			//Console.WriteLine("До обновления");
-			//foreach (var user in db.GetTable<User>().Take(5))
-			//{
-			//	Console.WriteLine("{0} \t{1}", user.Id, user.name);
-			//}
-			//Console.WriteLine();
-
-			//// возьмем первого пользователя
-			//User user1 = db.GetTable<User>().FirstOrDefault();
-			//// и изменим у него возраст
-			//user1.Id = 11;
-			//// сохраним изменения
-			//db.SubmitChanges();
-
-			//Console.WriteLine();
-			//Console.WriteLine("После обновления");
-			//foreach (var user in db.GetTable<User>().Take(5))
-			//{
-			//	Console.WriteLine("{0} \t{1}", user.Id, user.name);
-			//}
-			//DataBase bd = new DataBase();
-			//string[] str = bd.GetUsers();
-			//Console.WriteLine(str);
-			//string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\228\Desktop\WOZAP\DataBase\Database1.mdf;Integrated Security=True";
-
-			////string[] str = { "123", "123" };
-			//DataClasses1DataContext db = new DataClasses1DataContext(connectionString);
-
-			//// Получаем таблицу пользователей
-			//Table<User> users = db.GetTable<User>();
-			//int max_id = 1;
-			//int i = 0;
-			//string[] str = new string[max_id];
-			//foreach (var user in users)
-			//{
-			//	//Console.WriteLine("{0} \t{1}", user.Id, user.name);
-			//	if (i >= max_id)
-			//	{
-			//		string[] new_str = new string[max_id + 100];
-
-			//		for (int j = 0; j < max_id; j++)
-			//			new_str[j] = str[j];
-
-			//		max_id += 100;
-			//		str = new_str;
-			//	}
-
-			//	str[i++] = user.name;
-			//	//Console.WriteLine(str[i - 1]);
-			//}
-			//foreach (string Str in str)
-			//	Console.WriteLine(Str);
-
 			//DataBase db = new DataBase();
-			//string[] str = db.GetUsers();
-			//foreach (string Str in str)
-			//	Console.WriteLine(Str);
+			//string from = "vadik";
+			//string [] a;
+			//string to = "sada";
+			//a= db.GetMsg(from, to);
 
+			//for (int i = 0; i <3; i++)
+			//{
+			//	Console.WriteLine(a[i]);
+			//}
 		}
 	}
 }
-
 
